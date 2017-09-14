f4e4ae65a4c16f5ad85a63e48955e0bdfc421c59
Rajon <1062324860@qq.com>
2017-09-15 00:05:39 +0800

    Update jiudu1456_bfs.cpp

--- jiudu1456_bfs.cpp
+++ jiudu1456_bfs.cpp
@@ -1,93 +1,93 @@
-////ac.jobdu.com/problem.php?pid=1456
-//#include<cstdio>
-//#include<iostream>
-//#include<cstring>
-//#include<string>
-//#include<cmath>
-//#include<cctype>
-//#include<algorithm>
-//#include<vector>
-//#include<set>
-//#include<map>
-//#include<queue>
-//#include<stack>
-//#include<fstream>
-//using namespace std;
-//const int maxn=10000;
-//int X[6]={0,0,0,0,1,-1};
-//int Y[6]={0,0,1,-1,0,0};
-//int Z[6]={1,-1,0,0,0,0};
-//bool vis[51][51][51]={false};
-//int maze[51][51][51]={};
-//int n,m,t,T;
-//bool test(int x,int y,int z){
-//	if(x<0 || x>=n||y<0||y>=m||z>=t||z<0|| maze[x][y][z]==1)
-//		return false;
-//	return true;
-//}
-//struct node{
-//	int x, y,z;
-//	int step;//Ò»¶¨Òª¶¨ÒåÎªnodeµÄ³ÉÔ±£¬½ÚµãÔË¶¯ÖĞ¼ÇÂ¼×Å²½Êı¡£
-//};
-//int bfs(int n,int m,int t){
-//	queue<node> q;
-//	node st;
-//	st.x=0;
-//	st.y=0;
-//	st.z=0;
-//	vis[0][0][0]=true;
-//	st.step=0;		//Èôµ¥¶À¶¨ÒåstepÎŞ·¨Óë×ßµ½µÄ½Úµã´¦µÄÕæÊµ²½Êı±£³ÖÒ»ÖÂ
-//	q.push(st);
-//
-//	while(!q.empty()){
-//		node top=q.front();
-//		q.pop();
-//		if(top.x==n-1 && top.y==m-1 && top.z==t-1)
-//			return top.step;
-//		for(int i=0;i<6;i++){
-//			int nx=top.x+X[i];
-//			int ny=top.y+Y[i];
-//			int nz=top.z+Z[i];
-//			int nstep=top.step+1; //²»¿ÉĞ´³É++£»
-//			if(test(nx,ny,nz)){
-//				node nt;
-//				nt.x=nx;
-//				nt.y=ny;
-//				nt.z=nz;
-//				nt.step=nstep;
-//				if(vis[nx][ny][nz]==false){
-//					vis[nx][ny][nz]=true;
-//					q.push(nt);
-//					//cout<<nx<<" "<<ny<<" "<<nz<<" "<<nstep<<endl;
-//				}
-//			}
-//		}
-//	}
-//	return -1;
-//}
-//int k;
-//int main(){
-//#ifdef _DEBUG
-//	ifstream cin("a.txt");
-//#endif
-//	cin>>k;
-//	while(k--){
-//		memset(vis,false,sizeof(vis));
-//		cin>>n>>m>>t>>T;
-//		for(int x=0;x<n;x++){
-//			for(int y=0;y<m;y++)
-//				for(int z=0;z<t;z++)
-//					cin>>maze[x][y][z];
-//		}
-//	
-//		int ans=bfs(n,m,t);
-//		if(ans>T)
-//			cout<<"-1"<<endl;
-//		else
-//			cout<<ans<<endl;
-//	}
-//#ifdef _DEBUG
-//	cin.close();
-//#endif
-//	return 0;
-//}
+//ac.jobdu.com/problem.php?pid=1456
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<cctype>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<fstream>
+using namespace std;
+const int maxn=10000;
+int X[6]={0,0,0,0,1,-1};
+int Y[6]={0,0,1,-1,0,0};
+int Z[6]={1,-1,0,0,0,0};
+bool vis[51][51][51]={false};
+int maze[51][51][51]={};
+int n,m,t,T;
+bool test(int x,int y,int z){
+	if(x<0 || x>=n||y<0||y>=m||z>=t||z<0|| maze[x][y][z]==1)
+		return false;
+	return true;
+}
+struct node{
+	int x, y,z;
+	int step;//ä¸€å®šè¦å®šä¹‰ä¸ºnodeçš„æˆå‘˜ï¼ŒèŠ‚ç‚¹è¿åŠ¨ä¸­è®°å½•ç€æ­¥æ•°ã€‚
+};
+int bfs(int n,int m,int t){
+	queue<node> q;
+	node st;
+	st.x=0;
+	st.y=0;
+	st.z=0;
+	vis[0][0][0]=true;
+	st.step=0;		//è‹¥å•ç‹¬å®šä¹‰stepæ— æ³•ä¸èµ°åˆ°çš„èŠ‚ç‚¹å¤„çš„çœŸå®æ­¥æ•°ä¿æŒä¸€è‡´
+	q.push(st);
+
+	while(!q.empty()){
+		node top=q.front();
+		q.pop();
+		if(top.x==n-1 && top.y==m-1 && top.z==t-1)
+			return top.step;
+		for(int i=0;i<6;i++){
+			int nx=top.x+X[i];
+			int ny=top.y+Y[i];
+			int nz=top.z+Z[i];
+			int nstep=top.step+1; //ä¸å¯å†™æˆ++ï¼›
+			if(test(nx,ny,nz)){
+				node nt;
+				nt.x=nx;
+				nt.y=ny;
+				nt.z=nz;
+				nt.step=nstep;
+				if(vis[nx][ny][nz]==false){
+					vis[nx][ny][nz]=true;
+					q.push(nt);
+					//cout<<nx<<" "<<ny<<" "<<nz<<" "<<nstep<<endl;
+				}
+			}
+		}
+	}
+	return -1;
+}
+int k;
+int main(){
+#ifdef _DEBUG
+	ifstream cin("a.txt");
+#endif
+	cin>>k;
+	while(k--){
+		memset(vis,false,sizeof(vis));
+		cin>>n>>m>>t>>T;
+		for(int x=0;x<n;x++){
+			for(int y=0;y<m;y++)
+				for(int z=0;z<t;z++)
+					cin>>maze[x][y][z];
+		}
+	
+		int ans=bfs(n,m,t);
+		if(ans>T)
+			cout<<"-1"<<endl;
+		else
+			cout<<ans<<endl;
+	}
+#ifdef _DEBUG
+	cin.close();
+#endif
+	return 0;
+}
